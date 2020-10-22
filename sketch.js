0c0b9aa1917e7ed4077bba30bf3022b2c53e7481
Arushi2006 <72396009+Arushi2006@users.noreply.github.com>
2020-10-21 21:42:32 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,51 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+var thickness,wall;
+var speed,weight,bullet;
+
+function setup()
+{  
+    createCanvas(1600,400);
+    speed=random(223,321);
+    weight=random(32,52);
+    thickness=random(22,83);
+    bullet=createSprite(50,200,50,50);
+    bullet.shapeColor="white";
+    bullet.velocityX=speed;
+    wall=createSprite(1200,200,thickness,200);
+    wall.shapeColor=grey(80,80,80);
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function draw()
+{
+    background(0);
+    drawSprites;
+    c();
+}
+
+function c(bullet1,wall1)
+{
+  Mouse.y=bullet.y;
+  Mouse.x=bullet.x;
+
+bulletRightEdge=bullet1.x+bullet1.width;
+wallLeft=wall1.x;
+if(bulletRightEdge<=wallLeftEdge)
+   {
+      return true
+   }  
+       return false;
+  
+if(c(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5*32*223*223/70*70*70;
+
+  if(damage>10)
+  {
+    wall.shapeColor=red(225,0,0);
+  }
+   if(damage<10)
+   {
+      wall.shapeColor=green(0,225,0);
+   }
+}
 }
